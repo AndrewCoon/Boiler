0a05387309f3902d89357ee9a16bb36c4cafd6ab
Androo <84476053+Anduwu1@users.noreply.github.com>
2026-02-01 18:21:05 -0700

    Initial commit

---
+++ Boiler/Headers/boiler.hpp
@@ -0,0 +1,15 @@
+// Preprocessor Directives
+#ifndef BOILER
+#define BOILER
+#pragma once
+
+// Headers
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Some Constants
+const int mWidth = 1280;
+const int mHeight = 800;
+
+#endif // Header
